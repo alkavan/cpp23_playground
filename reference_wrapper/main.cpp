1473cd6ffb31138e3e7f1802a64f952f4b25e88e
alkavan <igal@alkontek.com>
2025-10-04 12:49:50 +0300

    adding reference_wrapper to the project.

---
+++ reference_wrapper/main.cpp
@@ -0,0 +1,33 @@
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <list>
+#include <numeric>
+#include <random>
+#include <vector>
+
+void println(auto const rem, std::ranges::range auto const& v)
+{
+    for (std::cout << rem; auto const& e : v)
+        std::cout << e << ' ';
+    std::cout << '\n';
+}
+
+int main()
+{
+    std::list<int> l(10);
+    std::iota(l.begin(), l.end(), -4);
+
+    // can't use shuffle on a list (requires random access), but can use it on a vector
+    std::vector<std::reference_wrapper<int>> v(l.begin(), l.end());
+
+    std::ranges::shuffle(v, std::mt19937{std::random_device{}()});
+
+    println("Contents of the list: ", l);
+    println("Contents of the list, as seen through a shuffled vector: ", v);
+
+    std::cout << "Doubling the values in the initial list...\n";
+    std::ranges::for_each(l, [](int& i) { i *= 2; });
+
+    println("Contents of the list, as seen through a shuffled vector: ", v);
+}
